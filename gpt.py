59e4eb6e8a944378f97da44e0d339636a3d18337
Samip Gyawali <samipthegreat@gmail.com>
2023-04-24 11:28:49 +0545

    JARVIS

--- gpt.py
+++ gpt.py
@@ -3,18 +3,22 @@ import openai as oa
 
 oa.api_key = env.dotenv_values()['apikey']
 
+def askGpt(user_prompt):
+    response = oa.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {"role":"user","content":"You're JARVIS, and you will answer with JARVIS's intelligence, wit and humor. You will also refer to me as sir"},
+            {"role":"system","content":"Welcome back, sir!"},
+            {"role":"user","content":"I will ask you a few questions, and please answer politely"},
+            {"role":"system","content":"Sure, sir! Ask me the questions. I will only reply things I know for a fact. If not, I will add 'I don't know much about this but to my knowledge' before I answer"},
+            {"role":"user","content":user_prompt}
+        ])
+    message = response.choices[0].message.content
+    return  message
+
+
 user_prompt = input("Enter the question: ")
-
-response = oa.ChatCompletion.create(
-    model='gpt-3.5-turbo',
-    messages=[
-    {"role":"user","content":"You're Richard Feynman, and you will answer with Feynman's intelligence, wit and humor."},
-    {"role":"system","content":"Thank you! You're the good user!"},
-    {"role":"user","content":"I will ask you a few questions, and please answer politely"},
-    {"role":"system","content":"Sure! Tell me the questions. I will only reply things I know for a fact. If not, I will add 'I don't know much about this but to my knowledge' before I answer"},
-    {"role":"user","content":user_prompt}
-])
-
+message = askGpt(user_prompt)
 with open('responses.txt','a') as responseFile:
     responseFile.write(f"\nprompt: {user_prompt}\n")
-    responseFile.write(f"Reponse: {response.choices[0].message.content}")
+    responseFile.write(f"Reponse: {message}")
