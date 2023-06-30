214413f617c0399144f80c8deff2023172698b11
unknown <samipthegreat@gmail.com>
2023-06-30 20:11:15 +0545

    Windows optimization

--- stt.py
+++ stt.py
@@ -1,14 +1,15 @@
 import speech_recognition as sr
+import tts
 
 def listen():
     global recognizer
     try:
         with sr.Microphone() as source:
-            print("You may now speak!")
+            tts.speak('You may now speak')
             audio = recognizer.record(source,duration=5)
             text = recognizer.recognize_google(audio)
     except Exception as e:
-        print("An exception occured")
+        print(e)
         return "stop"
     return text
 
